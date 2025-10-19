1ffaa5c10f88523e36e350aa3d83c8dab08dea57
Haytam RHALLAB <161653466+Jaguaarr@users.noreply.github.com>
2025-10-19 10:36:15 +0100

    Refactor Instructor class package declaration

--- challenge/src/main/java/problem5/Instructor/Instructor.java
+++ challenge/src/main/java/problem5/Instructor/Instructor.java
@@ -1,4 +1,4 @@
-package instructor;
+package Instructor;
 
 public class Instructor extends Person {
     private String employeeNumber;
@@ -56,3 +56,4 @@ public class Instructor extends Person {
 
 
 }
+
